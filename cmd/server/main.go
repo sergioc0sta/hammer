f3deedede899bb9da2e2fd1bbad8666d29a3dd42
Sérgio Costa <sergio.md.costa@protonmail.com>
2026-01-08 15:50:42 +0000

    feat(request): Create simpre paralele request with goroutines

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/sergioc0sta/hammer/internal/cli"
 	"github.com/sergioc0sta/hammer/internal/infra/handler"
@@ -9,19 +10,30 @@ import (
 
 func main() {
 
+	var wg sync.WaitGroup
 	prs, err := cli.InsertParameters()
 
-	if(err != nil){
-    panic(err)
+	if err != nil {
+		panic(err)
 	}
 
 	fmt.Println("URL:", prs.URL)
 	fmt.Println("Requests:", prs.Requests)
 	fmt.Println("Concurrency:", prs.Concurrency)
-	status, err := handler.ServerRequestHandler(prs.URL)
-
-	fmt.Println("STATUS:", status)
-	fmt.Println("ERR:", err)
-
 	fmt.Println("Server is starting...")
+
+	for i := 0; i < prs.Concurrency; i++ {
+
+		wg.Add(1)
+		go func(numberRequests int) {
+			defer wg.Done()
+
+			for i := 0; i < numberRequests; i++ {
+				handler.ServerRequestHandler(prs.URL)
+
+			}
+		}(prs.Requests)
+	}
+
+	wg.Wait()
 }
